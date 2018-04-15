b57a99e8d6ae42508feeb536154dc5703da3643e
vermosen <vermosen.jm@free.fr>
2018-04-15 14:41:10 -0400

    fixed parseTime3

--- parseTime3/main.cpp
+++ parseTime3/main.cpp
@@ -7,6 +7,19 @@
 namespace qi = boost::spirit::qi;
 namespace phx = boost::phoenix;
 
+namespace boost { namespace spirit { namespace traits {
+	template <>
+	struct transform_attribute<unsigned, std::vector<char>, qi::domain> {
+		typedef std::vector<char> type;
+		static type pre(unsigned& d) { return {}; }
+		static void post(unsigned& val, const type& attr) {
+			for (std::size_t i = 0; i < attr.size(); i++)
+				val = 10 * val + attr[i] - '0';
+		}
+		static void fail(unsigned&) {}
+	};
+}}}
+
 int main() {
 
 	// goal: read a time string in format HH:MM:SS.ffff
@@ -17,13 +30,47 @@ int main() {
 
 	boost::fusion::vector<unsigned, unsigned, unsigned, unsigned> args;
 
+	unsigned total = 10e8;
 	bool res = qi::phrase_parse(
 		test.cbegin(), test.cend()
 		, qi::no_skip[qi::uint_[qi::_pass = (qi::_1 < 24)]
 		>> ':' >> qi::uint_[qi::_pass = (qi::_1 < 60)]
 		>> ':' >> qi::uint_[qi::_pass = (qi::_1 < 60)]
-		>> (qi::eps(phx::ref(precision) > 0) >> '.' >> qi::uint_[qi::repeat(4)[qi::char_]])]
+		>> (qi::eps(phx::ref(precision) > 0) >> '.' >> 
+			qi::attr_cast(qi::repeat(phx::ref(precision))[qi::char_]))]
 		, qi::space, args);
 
 	return 0;
 }
+
+/*
+#include <tuple>
+
+#include <boost/config/warning_disable.hpp>
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix.hpp>
+
+namespace qi = boost::spirit::qi;
+namespace phx = boost::phoenix;
+
+int main() {
+
+// goal: read a time string in format HH:MM:SS.ffff
+// given a precision and add the corresponding ticks
+std::string test = "10:56:10.12344";
+
+unsigned int precision = 3;
+
+boost::fusion::vector<unsigned, unsigned, unsigned, std::string> args;
+
+bool res = qi::phrase_parse(
+	test.cbegin(), test.cend()
+	, qi::no_skip[qi::uint_[qi::_pass = (qi::_1 < 24)]
+	>> ':' >> qi::uint_[qi::_pass = (qi::_1 < 60)]
+	>> ':' >> qi::uint_[qi::_pass = (qi::_1 < 60)]
+	>> (qi::eps(phx::ref(precision) > 0) >> '.' >> qi::as_string[qi::repeat(4)[qi::char_]])]
+	, qi::space, args);
+
+return 0;
+}
+*/
